f3b2a70105c50c64097372893363e3bb973a9479
Orewa-veer <pratiharvirendra@gmail.com>
2025-06-23 23:43:26 +0530

    Understanding http and npm

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-const { error } = require('console');
-const fs= require('fs');
-fs.readdir('./',(error,file)=>{
-    if(error) console.log(error);
-    else console.log(file);
-    
-})
+const http = require("http");
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write("Hello World");
+    res.end();
+  }
+  if (req.url === "/api/courses") {
+    res.write(JSON.stringify([1, 2, 3]));
+    res.end();
+  }
+});
+server.on("connection", (socket) => {
+  console.log("New Connection...");
+});
+server.listen(3000);
+console.log("Listening on port 3000...");
