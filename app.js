a24e1db748f829e07dd7a9566b2d71a0870b6481
Orewa-veer <pratiharvirendra@gmail.com>
2025-06-26 10:26:06 +0530

    Path module

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
-const http = require("http");
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.write("Hello World");
-    res.end();
-  }
-  if (req.url === "/api/courses") {
-    res.write(JSON.stringify([1, 2, 3]));
-    res.end();
-  }
-});
-server.on("connection", (socket) => {
-  console.log("New Connection...");
-});
-server.listen(3000);
-console.log("Listening on port 3000...");
+const path = require("path");
+const pathObj = path.parse(__filename);
+console.log(pathObj);
