7ab6f5376cf9baf85e15d5d6e77b856c9530f8a4
Orewa-veer <pratiharvirendra@gmail.com>
2025-06-26 10:31:29 +0530

    OS module

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
-const path = require("path");
-const pathObj = path.parse(__filename);
-console.log(pathObj);
+const os = require("os");
+const freeMem = os.freemem();
+const totalMem = os.totalmem();
+const uptime = os.uptime();
+console.log(freeMem);
+console.log(totalMem);
+console.log(uptime);
